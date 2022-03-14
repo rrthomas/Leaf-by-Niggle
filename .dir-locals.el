e0ad9ddc5d3acee64d894abf84fcdb8e60f05612
Reuben Thomas <rrt@sc3d.org>
2022-03-14 10:42:16 +0000

    .dir-locals.el: update Dropbox version on save

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,5 @@
 ((latex-mode . ((eval . (add-hook 'after-save-hook
-                                  (lambda () (TeX-command-menu "LatexMk"))
+                                  (lambda () (progn
+                                               (TeX-command-menu "LatexMk")
+                                               (copy-file "Leaf by Niggle.pdf" "~/Dropbox/Niggle/Leaf by Niggle.pdf" t)))
                                   nil t)))))
