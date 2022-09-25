fc5ad43daf7254354eee6097d5d594f32c95a870
Reuben Thomas <rrt@sc3d.org>
2022-09-25 18:57:15 +0200

    .dir-locals.el: factor out a string constant (directory)

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,9 +3,10 @@
                     (add-hook 'after-save-hook
                               (lambda ()
                                 ;; Save buffer-file-name's value, as buffer is changed by TeX-command-menu
-                                (let ((file (buffer-file-name)))
+                                (let ((file (buffer-file-name))
+                                      (dropbox-directory "~/Dropbox/Niggle/"))
                                   (progn
                                     (start-process "latexmk" nil "latexmk")
-                                    (copy-file "book.pdf" "~/Dropbox/Niggle/" t t)
-                                    (copy-file "score.pdf" "~/Dropbox/Niggle/" t t))))
+                                    (copy-file "book.pdf" dropbox-directory t t)
+                                    (copy-file "score.pdf" dropbox-directory t t))))
                               nil t)))))
