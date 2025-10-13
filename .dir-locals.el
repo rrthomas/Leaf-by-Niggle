09c46c0bd768c78dfad2f8f9efa1e252e7eb4b92
Reuben Thomas <rrt@sc3d.org>
2025-10-13 20:18:26 +0100

    .dir-locals.el: no longer copy files to Dropbox

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,11 +2,5 @@
              ((eval .
                     (add-hook 'after-save-hook
                               (lambda ()
-                                ;; Save buffer-file-name's value, as buffer is changed by TeX-command-menu
-                                (let ((file (buffer-file-name))
-                                      (dropbox-directory "~/Dropbox/Niggle/"))
-                                  (progn
-                                    (start-process "latexmk" nil "latexmk")
-                                    (copy-file "book.pdf" dropbox-directory t t)
-                                    (copy-file "score.pdf" dropbox-directory t t))))
+                                (start-process "latexmk" nil "latexmk"))
                               nil t)))))
